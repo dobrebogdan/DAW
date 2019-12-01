dc00303feb0060941dd77cabe29cc50057acad28
dobrebogdan <36445756+dobrebogdan@users.noreply.github.com>
2019-12-01 14:00:14 +0200

    Added controllers

--- DAW/DAW/App_Start/RouteConfig.cs
+++ DAW/DAW/App_Start/RouteConfig.cs
@@ -11,6 +11,18 @@ namespace DAW {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Categories",
+                url: "categories",
+                defaults: new { controller = "Category", action = "Index"}
+            );
+
+            routes.MapRoute(
+                name: "Subject",
+                url: "{category}/{subject}",
+                defaults: new { controller = "Subject", action = "Index"}
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
